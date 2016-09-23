5a43626b7c215b73c6575d2ee2b65670601386ee
John <jcharante@gmail.com>
2016-09-23 15:04:46 -0400

    Created pset1.py

---
+++ pset1.py
@@ -0,0 +1,19 @@
+# Read Lesson 1 First
+# 
+
+# Problem 1.
+a = 100
+a = a - 50
+
+# True or False?
+a == 50
+# Answer Here:
+
+# Problem 2.
+b = 50
+b = b - a
+
+# True or False?
+b == 50
+# Answer Here:
+

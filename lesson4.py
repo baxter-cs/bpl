ee0d448f744b5294451449edc029fca206015d2e
John <jcharante@gmail.com>
2016-09-23 14:53:47 -0400

    Added lesson 4

---
+++ lesson4.py
@@ -0,0 +1,28 @@
+# Lesson 4
+# Operators
+
+a = 5
+
+a += 2   # Equiv. to a = a + 2
+
+print(a) # 7
+
+a -= 2   # Equiv. to a = a - 2
+
+print(a) # 5
+
+a *= 2   # Equiv. to a = a * 2
+
+print(a) # 10
+
+a /= 2   # Equiv. to a = a / 2
+
+print(a) # 5
+
+a **= 2  # Equiv. to a = a**2
+
+print(a) # 25
+
+a %= 2   # Equiv. to a = a % 2
+
+print(a) # 1

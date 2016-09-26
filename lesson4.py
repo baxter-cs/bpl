0b8dfafe538bf8530e59b18cd68cb54e278e2f92
halstedlarsson <halsted.larsson@gmail.com>
2016-09-25 20:43:14 -0400

    broke lesson 4 out into operators and assignemnt
    broke it out into two sections, added challenge problems

--- lesson4.py
+++ lesson4.py
@@ -1,28 +1,58 @@
-# Lesson 4
-# Operators
+# Lesson 4, Operators
+#part 1
+
+print(2 + 2) #prints 2
+print(2 - 2) #prints 0
+print(2 * 2) #prints 4
+print(2 / 2) #what will this print?
+
+print(2 ** 2) #prints 4
+print(2 ** 3) #prints 8
+print(2 ** 4) #prints 16
+print(2 ** 5) #what will this print?
+print(3 ** 2) #what will this print?
+
+print(2 % 2) #prints 0
+print(3 % 2) #prints 1
+print(4 % 2) #prints 0
+print(7 % 2) #prints 1
+
+print(10 % 3) #prints 1
+print(11 % 3) #prints 2
+print(12 % 3) #prints 0
+print (13 % 3) #what will this print?
+print (13 % 2) #what will this print?
+print (16 % 5) #what will this print?
+
+#lesson 4.5, assignment
 
 a = 5
+print(a) #prints 5
 
-a += 2   # Equiv. to a = a + 2
+a = 5
+a = a + 2
+print(a) #prints 7
 
-print(a) # 7
+a = 5
+a += 2
+print(a) #prints 7
 
-a -= 2   # Equiv. to a = a - 2
+a = 3
+a -= 3
+print(a) #what does this print?
 
-print(a) # 5
+a = 5
+a *= 2
+print(a) # prints 10
 
-a *= 2   # Equiv. to a = a * 2
+a = 10
+a /= 2
+print(a) # prints 5
 
-print(a) # 10
+a = 5
+a **= 2
+print(a) # what does this print?
 
-a /= 2   # Equiv. to a = a / 2
-
-print(a) # 5
-
-a **= 2  # Equiv. to a = a**2
-
-print(a) # 25
-
-a %= 2   # Equiv. to a = a % 2
-
-print(a) # 1
+a = 5
+a %= 2
+print(a)  # what does this print?
